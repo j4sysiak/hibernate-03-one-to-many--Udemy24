1d41945282d925dac0492cc35bd8cfa6040d829c
Jacek <Jacek@192.168.1.100>
2020-02-29 09:27:36 +0100

    bvbvbvb

--- hibernate-03-one-to-many--Udemy24/src/pl/jaceksysiak/hibernate/demo/entity/Instructor.java
+++ hibernate-03-one-to-many--Udemy24/src/pl/jaceksysiak/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package pl.jaceksysiak.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -46,6 +50,12 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 	
+	@OneToMany(mappedBy="instructor",
+			   cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+						 CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Course> courses;
+	
+
 	public Instructor() {
 		
 	}
@@ -95,11 +105,27 @@ public class Instructor {
 	public void setInstructorDetail(InstructorDetail instructorDetail) {
 		this.instructorDetail = instructorDetail;
 	}
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
 
-	@Override
-	public String toString() {
-		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", instructorDetail=" + instructorDetail + "]";
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	
+	// add convenience methods for bi-directional relationship
+	
+	public void add(Course tempCourse) {
+		
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
 	}
 	
 	
